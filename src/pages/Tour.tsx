e3938a6b70bce5d22502f941491bbd0645b631c2
passaondigital <passaondigital@gmail.com>
2026-08-15 16:57:10 +0200

    feat(tour): surface live tour editing

--- src/pages/Tour.tsx
+++ src/pages/Tour.tsx
@@ -1,11 +1,13 @@
 import { SlimTourScreen } from "@/components/slim/SlimTourScreen";
 import { TourArrivalControl } from "@/components/slim/TourArrivalControl";
+import { TourLiveEditControl } from "@/components/slim/TourLiveEditControl";
 import { TourQuickGuide } from "@/components/slim/TourQuickGuide";
 
 const TourPage = () => (
   <>
     <TourQuickGuide />
     <TourArrivalControl />
+    <TourLiveEditControl />
     <SlimTourScreen />
   </>
 );
