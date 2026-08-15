0055ae57b44ef76ee8bb1c7ac227604fda434002
passaondigital <passaondigital@gmail.com>
2026-08-15 14:52:08 +0200

    fix(tour): use one tour engine for all provider routes

--- src/pages/Tour.tsx
+++ src/pages/Tour.tsx
@@ -1,26 +1,11 @@
-import { useLocation } from "react-router-dom";
-import { DayCockpit } from "@/components/day-cockpit";
-import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
 import { SlimTourScreen } from "@/components/slim/SlimTourScreen";
 import { TourQuickGuide } from "@/components/slim/TourQuickGuide";
 
-const TourPage = () => {
-  const location = useLocation();
-
-  if (location.pathname === "/home/tour") {
-    return (
-      <>
-        <TourQuickGuide />
-        <SlimTourScreen />
-      </>
-    );
-  }
-
-  return (
-    <ProtectedRoute allowedRoles={["provider", "admin"]}>
-      <DayCockpit />
-    </ProtectedRoute>
-  );
-};
+const TourPage = () => (
+  <>
+    <TourQuickGuide />
+    <SlimTourScreen />
+  </>
+);
 
 export default TourPage;
