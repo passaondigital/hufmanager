6521399d2ac4b01fad60d56e4c19f8fafd99ac78
passaondigital <passaondigital@gmail.com>
2026-08-15 15:20:49 +0200

    feat(tour): surface explicit arrived action

--- src/pages/Tour.tsx
+++ src/pages/Tour.tsx
@@ -1,9 +1,11 @@
 import { SlimTourScreen } from "@/components/slim/SlimTourScreen";
+import { TourArrivalControl } from "@/components/slim/TourArrivalControl";
 import { TourQuickGuide } from "@/components/slim/TourQuickGuide";
 
 const TourPage = () => (
   <>
     <TourQuickGuide />
+    <TourArrivalControl />
     <SlimTourScreen />
   </>
 );
